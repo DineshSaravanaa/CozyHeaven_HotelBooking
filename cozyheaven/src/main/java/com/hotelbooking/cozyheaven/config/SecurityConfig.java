f83d33935d0f197183771e2b72d6676518611eba
DhineshKumar <dhineshgokul03@gmail.com>
2025-05-06 14:49:20 +0530

    Added the Global Config for CORS

--- cozyheaven/src/main/java/com/hotelbooking/cozyheaven/config/SecurityConfig.java
+++ cozyheaven/src/main/java/com/hotelbooking/cozyheaven/config/SecurityConfig.java
@@ -1,19 +1,23 @@
 package com.hotelbooking.cozyheaven.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.hotelbooking.cozyheaven.service.MyUserService;
 
@@ -28,7 +32,9 @@ public class SecurityConfig {
 
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http.cors(Customizer.withDefaults()).csrf(csrf -> csrf.disable())
+		http
+		.cors(withDefaults())
+		.csrf(csrf -> csrf.disable())
 				.authorizeHttpRequests((authorize) -> authorize.requestMatchers("api/auth/signup").permitAll()
 						.requestMatchers("/api/auth/login").authenticated()
 						.requestMatchers("/api/auth/token/generate").permitAll()
@@ -91,17 +97,27 @@ public class SecurityConfig {
 						.requestMatchers("/api/cancellationrequest/getall").hasAuthority("Admin")
 						.requestMatchers("/api/admin/getadmin").hasAuthority("Admin")
 						.requestMatchers("/api/admin/profile/update").hasAuthority("Admin")
+						.requestMatchers("/api/refund/getall").hasAnyAuthority("Admin","HotelOwner")
+						.requestMatchers("/swagger-ui/**").permitAll()
 						
 						
 						
-						
-						
-						.anyRequest().authenticated()
+						.anyRequest().permitAll()
 						)
 				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 				.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
 		return http.build();
+	}
+	@Bean
+	UrlBasedCorsConfigurationSource corsConfigurationSource() {
+	    CorsConfiguration configuration = new CorsConfiguration();
+	    configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+	    configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE","OPTIONS"));
+	    configuration.setAllowedHeaders(Arrays.asList("*"));
+	    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	    source.registerCorsConfiguration("/**", configuration);
+	    return source;
 	}
 
 	@Bean
